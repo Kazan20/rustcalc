8671403b8d204991cea5840b370c552d8dafe043
Ariel Zvinowanda <kazan@outlook.ph>
2025-08-27 20:26:56 +0200

    Improve user prompts and enhance multiplication functionality in calculator

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,34 @@
+use std::io::{self, Write};
 use text_io::read;
 
 fn main() {
     print!("Enter a number: ");
+    io::stdout().flush().unwrap();
     let num1: f64 = read!();
 
-    print!("Enter a operator: ");
+    print!("Enter an operator (+, -, *, /): ");
+    io::stdout().flush().unwrap();
     let op: char = read!();
 
     print!("Enter a number: ");
+    io::stdout().flush().unwrap();
     let num2: f64 = read!();
 
     let result: f64 = match op {
         '+' => num1 + num2,
         '-' => num1 - num2,
-        '*' => num1 * num2,
+        '*' => {
+            let res = num1 * num2;
+            println!("{}", res);
+            print!("Times result by PI? (y/n): ");
+            io::stdout().flush().unwrap();
+            let time_pi: char = read!();
+
+            match time_pi {
+                'y' | 'Y' => res * std::f64::consts::PI,
+                _ => res,
+            }
+        }
         '/' if num2 != 0.0 => num1 / num2,
         '/' => {
             eprintln!("Error: Division by zero!");
@@ -25,5 +40,5 @@ fn main() {
         }
     };
 
-    println!("Result: {}", result)
+    println!("Result: {}", result);
 }

442436d52fba8e2cdf825f4c145547e6af01e7f6
Ariel Zvinowanda <kazan@outlook.ph>
2025-08-28 15:36:00 +0200

    Made unit converter instead

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,72 @@
-use std::io::{self, Write};
 use text_io::read;
+use std::io::{self, Write};
 
 fn main() {
-    print!("Enter a number: ");
-    io::stdout().flush().unwrap();
-    let num1: f64 = read!();
+    let choice: i32;
 
-    print!("Enter an operator (+, -, *, /): ");
-    io::stdout().flush().unwrap();
-    let op: char = read!();
+    println!("**********************");
+    println!("*** Unit Converter ***");
+    println!("**********************");
 
-    print!("Enter a number: ");
-    io::stdout().flush().unwrap();
-    let num2: f64 = read!();
+    println!("1. Temperature");
+    println!("2. Weight");
+    println!("3. TODO"); // TODO: make option
 
-    let result: f64 = match op {
-        '+' => num1 + num2,
-        '-' => num1 - num2,
-        '*' => {
-            let res = num1 * num2;
-            println!("{}", res);
-            print!("Times result by PI? (y/n): ");
-            io::stdout().flush().unwrap();
-            let time_pi: char = read!();
+    print!("> ");
+    choice = read!();
+    io::stdout().flush().expect("Failed to flush stdout");
 
-            match time_pi {
-                'y' | 'Y' => res * std::f64::consts::PI,
-                _ => res,
+    match choice {
+        1 => {
+            let fahrenheit: f64;
+            let celsius: f64;
+            let choice_t: i32;
+
+            println!("1. Fahrenheit -> Celsius. 2. Celsius -> Fahrenheit");
+            print!("> ");
+            choice_t = read!();
+
+            if choice_t == 1 {
+                print!("Enter the amount in Fahrenheit: ");
+                fahrenheit = read!();
+                io::stdout().flush().expect("Failed to flush stdout");
+
+                celsius = (fahrenheit - 32.0) / 1.8;
+                println!("Result: {}", celsius)
+            } else if choice_t == 2 {
+                print!("Enter the amount in Celsius: ");
+                celsius = read!();
+                io::stdout().flush().expect("Failed to flush stdout");
+
+                fahrenheit = (celsius * 1.8) + 32.0;
+                println!("Result: {}", fahrenheit);
             }
         }
-        '/' if num2 != 0.0 => num1 / num2,
-        '/' => {
-            eprintln!("Error: Division by zero!");
-            return;
-        }
-        _ => {
-            eprintln!("Error: Invalid operator!");
-            return;
-        }
-    };
+        2 => {
+            let pounds: f64;
+            let kilograms: f64;
+            let choice_w: i32;
+            println!("1. Pounds -> Kilograms. 2. Kilograms -> Pounds");
 
-    println!("Result: {}", result);
+            print!("> ");
+            choice_w = read!();
+
+            if choice_w == 1 {
+                print!("Enter the amount in Pounds: ");
+                pounds = read!();
+                io::stdout().flush().expect("Failed to flush stdout");
+
+                kilograms = pounds / 2.20462;
+                println!("Result: {}", kilograms)
+            } else if choice_w == 2 {
+                print!("Enter the amount in Kilograms: ");
+                kilograms = read!();
+                io::stdout().flush().expect("Failed to flush stdout");
+
+                pounds = kilograms * 2.20462;
+                println!("Result: {}", pounds);
+            }
+        }
+        _ => println!("Invalid Choice!"),
+    }
 }
